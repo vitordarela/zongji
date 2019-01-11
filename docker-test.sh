0fbfd72c26b1ac71c9b1cccdf7bd5d6dd951bb2a
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2019-01-11 12:37:02 -0200

    Preparing for testing with mysql 8

--- docker-test.sh
+++ docker-test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 MYSQL_HOSTS="mysql55 mysql56 mysql57 mysql80"
 
+docker-compose up -d
+
 for hostname in ${MYSQL_HOSTS}; do
   echo $hostname
   docker run -it --network=zongji_default -e MYSQL_HOST=$hostname -w /build -v $PWD:/build node:8 npm test
